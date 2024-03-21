cd1d9ae72a30657c99d31816b24ffd602a556610
JuJu222 <japjustin@yahoo.com>
2024-03-21 18:21:53 +0700

    temp

--- src/prisma/seeders/currency.ts
+++ src/prisma/seeders/currency.ts
@@ -3,6 +3,7 @@ import {PrismaClient} from "@prisma/client";
 export async function seedCurrencies(prisma: PrismaClient) {
     await prisma.currency.create({
         data: {
+            id: "1",
             name: "United States Dollars",
             code: "USD",
             symbol: "$",
